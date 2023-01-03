840dbe4f5ea86b11bd1370fcedb45a8c420ba61b
Riyan Wibowo <tunggalrahayu1@gmail.com>
2023-01-03 22:02:31 +0700

    fix

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -2,34 +2,42 @@ json = JSON.parse(content)['data']
 
 vars = page['vars']
 
-products = json['results']
+if json['results'] or json['pagination']
+    products = json['results']
 
-if products.count > 0
-    products.each_with_index do |prod, i|
-        pages << {
-            url: prod['url'],
-            page_type: "products",
-            vars: vars.merge("rank"=>(i+1), "prod"=>prod, "listings_gid"=>page['gid'])
+    if products.count > 0
+        products.each_with_index do |prod, i|
+            pages << {
+                url: prod['url'],
+                page_type: "products",
+                vars: vars.merge("rank"=>(i+1), "prod"=>prod, "listings_gid"=>page['gid'])
+            }
+        end
+    else
+        outputs << {
+            _collection: "subcat_dont_have_product",
+            vars: vars
         }
+    end
+
+
+    if vars['pn'] == 1
+        total = json['pagination']['count'].to_f
+        perpage = json['pagination']['perPage'].to_f
+        max_page = (total/perpage).ceil
+
+        (2..max_page).each do |pn|
+            pages << {
+                url: page['url'].gsub("page=1", "page=#{pn}"),
+                page_type: "listings",
+                vars: vars.merge("pn"=>pn)
+            }
+        end
     end
 else
-    outputs << {
-        _collection: "subcat_dont_have_product",
+    pages << {
+        url: vars['ori_url'],
+        page_type: "listing_html",
         vars: vars
     }
-end
-
-
-if vars['pn'] == 1
-    total = json['pagination']['count'].to_f
-    perpage = json['pagination']['perPage'].to_f
-    max_page = (total/perpage).ceil
-
-    (2..max_page).each do |pn|
-        pages << {
-            url: page['url'].gsub("page=1", "page=#{pn}"),
-            page_type: "listings",
-            vars: vars.merge("pn"=>pn)
-        }
-    end
 end
