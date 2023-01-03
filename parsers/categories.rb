131e3d94e9166d092f4c4a9fdf25d649f46d4d29
Riyan Wibowo <tunggalrahayu1@gmail.com>
2023-01-03 21:16:09 +0700

    fix

--- parsers/categories.rb
+++ parsers/categories.rb
@@ -25,8 +25,13 @@ if categories.count > 0
                         cat3 = subcat2['item_name']
 
                         url = subcat2['item_url'].split('/')
-                        category_name = url.last
-                        category_id = url[-2]
+                        if url.count > 6
+                            category_name = url.last
+                            category_id = url[-2]
+                        else
+                            category_name = cat3.gsub(' ', '_')
+                            category_id = url.last
+                        end
 
                         pages << {
                             url: "https://www.falabella.com.pe/s/browse/v1/listing/pe?page=1&categoryId=#{category_id}&categoryName=#{category_name}&pgid=2&pid=799c102f-9b4c-44be-a421-23e366a63b82&zones=912_LIMA_2%2CURBANO_83%2COLVAA_36%2C912_LIMA_1%2C150101%2CIBIS_21%2CPERF_TEST%2C150000",
