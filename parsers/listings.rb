47c5d6a582264ea0a0afdde7d642af402ba4d942
Galang Dwi Juniar <galang@Galang.local>
2024-06-06 23:01:50 +0700

    first

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -10,53 +10,66 @@ if page['failed_response_status_code']
         }
     end
 else
-    json = JSON.parse(content)['data']
+    # json = JSON.parse(content)['data']
 
-    vars = page['vars']
+    # vars = page['vars']
 
-    if json['results'] or json['pagination']
-        products = json['results']
+    # if json['results'] or json['pagination']
+    #     products = json['results']
 
-        if products.count > 0
-            products.each_with_index do |prod, i|
-                pages << {
-                    url: prod['url'],
-                    page_type: "products",
-                    driver: {
-                        name: "from_api"
-                    },
-                    vars: vars.merge("rank"=>(i+1), "prod"=>prod, "listings_gid"=>page['gid'])
-                }
-            end
-        else
-            outputs << {
-                _collection: "subcat_dont_have_product",
-                vars: vars
-            }
-        end
+    #     if products.count > 0
+    #         products.each_with_index do |prod, i|
+    #             pages << {
+    #                 url: prod['url'],
+    #                 page_type: "products",
+    #                 driver: {
+    #                     name: "from_api"
+    #                 },
+    #                 vars: vars.merge("rank"=>(i+1), "prod"=>prod, "listings_gid"=>page['gid'])
+    #             }
+    #         end
+    #     else
+    #         outputs << {
+    #             _collection: "subcat_dont_have_product",
+    #             vars: vars
+    #         }
+    #     end
 
 
-        if vars['pn'] == 1
-            total = json['pagination']['count'].to_f
-            perpage = json['pagination']['perPage'].to_f
-            max_page = (total/perpage).ceil
+    #     if vars['pn'] == 1
+    #         total = json['pagination']['count'].to_f
+    #         perpage = json['pagination']['perPage'].to_f
+    #         max_page = (total/perpage).ceil
 
-            (2..max_page).each do |pn|
-                pages << {
-                    url: page['url'].gsub("page=1", "page=#{pn}"),
-                    page_type: "listings",
-                    http2: true,
-                    headers: page['headers'],
-                    vars: vars.merge("pn"=>pn)
-                }
-            end
-        end
-    else
-        pages << {
-            url: vars['ori_url'],
-            page_type: "listing_html",
-            no_redirect: true,
-            vars: vars
-        }
+    #         (2..max_page).each do |pn|
+    #             pages << {
+    #                 url: page['url'].gsub("page=1", "page=#{pn}"),
+    #                 page_type: "listings",
+    #                 http2: true,
+    #                 headers: page['headers'],
+    #                 vars: vars.merge("pn"=>pn)
+    #             }
+    #         end
+    #     end
+    # else
+    #     pages << {
+    #         url: vars['ori_url'],
+    #         page_type: "listing_html",
+    #         no_redirect: true,
+    #         vars: vars
+    #     }
+    # end
+    nokogiri = Nokogiri.HTML(content)
+    script_text = nokogiri.at_css('body > script#__NEXT_DATA__').text.strip
+    File.open("jobs1233.json","w") do |f|
+        f.write(JSON.pretty_generate(script_text))
+    end
+    json = JSON.parse(script_text)
+    File.open("jobs1234.json","w") do |f|
+        f.write(JSON.pretty_generate(json))
+    end
+    build_id = json['buildId']
+    File.open("baru.json","w") do |f|
+        f.write(JSON.pretty_generate(build_id))
     end
 end
